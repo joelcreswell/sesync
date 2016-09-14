ef0811b17ee759aea8e1d3b8db5a22757273aef9
Creswell <Creswell.Joel@epa.gov>
2016-09-14 14:00:17 -0400

    Updated code for lessons 1 & 2

--- lesson-1.R
+++ lesson-1.R
@@ -1,8 +1,8 @@
 ## Configure git
 
-system("git config --global user.name ...")
-system("git config --global user.email ...")
+system("git config --global user.name 'Joel Creswell'")
+system("git config --global user.email 'creswell.joel@epa.gov'")
 
 ## Set a new origin URL
 
-system('git remote set-url origin ...')
+system('git remote set-url origin https://github.com/joelcreswell/sesync.git')
