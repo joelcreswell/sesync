ea6be6ea58389a80e22427712b42fd17c351cd80
Creswell <Creswell.Joel@epa.gov>
2016-09-14 16:16:00 -0400

    Updating after completing lesson 2

--- lesson-2.R
+++ lesson-2.R
@@ -7,51 +7,57 @@ vals <- seq(from=1,
 
 ## Vectors
 
-counts ...
+counts <- c(4, 3, 7, 5)
 
 ## Lists
 
-... <- list(...)
-... <- ...(list(1, 2), c(3, 4))
+x <- list(list(1, 2), c(3, 4))
+y <- c(list(1, 2), c(3, 4))
 
 ## Factors
 
-education <- ...(c("college", "highschool", "college", "middle"),
-                 ... = c("middle", "highschool", ...),
-                 ...)
+education <- factor(c("college", "highschool", "college", "middle"),
+                 levels = c("middle", "highschool", "college"),
+                 ordered=TRUE)
 
 ## Data Frames
 
-... data.frame(...)
+df<- data.frame(education, counts)
 
 ## Exercise 1
 
-...
+species <- c("frog","zebra","blue","seven")
+count <- c(2,9,426,3)
+
+data <- data.frame(species, count)
 
 ## Names
 
-names(...) <- ...
+names(df) <- c("ed","ct")
 
 ## Subsetting ranges
 
 days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-weekdays <- ...
-...
+weekdays <- days[2:6]
+weekend <- days[c(1,7)]
 
 ## Exercise 2
 
-...
+weekdays1 <- days[-c(1,7)]
+
+mwf <- days[seq(2, 6, by = 2)]
 
 ## Anatomy of a function
 
-function(...) {
-  ...
-  return(...)
+first <- function(x) {
+  result <- x[1, 1]
+  return(result)
 }
 
 ## Exercise 3
+# Subset data frame to give the results "highschool", "college"
 
-...
+df[2:3,1]
 
 ## Distributions and statistics
 
@@ -77,9 +83,9 @@ requirements <- c('dplyr',
                   'sp',
                   'tidyr',
                   'tmap')
-missing <- setdiff(...,
+missing <- setdiff(requirements,
                    rownames(installed.packages()))
 
-if (...) {
+if (length(missing) != 0) {
   install.packages(missing)
 }
